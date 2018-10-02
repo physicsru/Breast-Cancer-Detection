422c4fec0baf3f041066e7104be738d6263a49b6
chenaoki <cheanoki@gmail.com>
2018-10-02 05:00:29 +0000

    reconstruct trainer

--- pyusct/trainer.py
+++ pyusct/trainer.py
@@ -1,4 +1,4 @@
-import os, sys, glob, time, math
+import os, sys, glob, time, math, json
 sys.path.append("../pyusct/")
 
 import numpy as np
@@ -19,27 +19,48 @@ class Trainer_prototype():
     
     def __init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, type, random_state):
         
-        dataset_name = dataset_dir.split('/')[-2]
-        self.output_path = os.path.join(model_output_path, dataset_name + "_" + type + '_lr_' + str(lr) + '_epoch_' + str(epochs) + '_batchsize_' + str(batch_size) + "_l2_alpha_" + str(l2_alpha) + '/')
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        self.output_path = os.path.join(model_output_path, timestr + "/")
+        
+        params = {
+            "dataset": dataset_dir,
+            "model_type": type,
+            "lr": lr,
+            "epoch": epochs,
+            "batchsize":batch_size,
+            "l2_alpha": l2_alpha,
+            "random_state": random_state,
+        }
+        params = json.dumps(params)
         
         if not os.path.exists(self.output_path):
             os.makedirs(self.output_path)
-        self.type = type
-        self.lr, self.epochs, self.batch_size, self.l2_alpha = lr, epochs, batch_size, l2_alpha
+        # save params
+        with open(os.path.join(output_path, 'params.json'), 'w') as outfile:
+            json.dump(params, outfile)
+        # initialization
+        self.lr, self.epochs, self.batch_size, self.l2_alpha, self.type = lr, epochs, batch_size, l2_alpha, type
         self.input_list = sorted(glob.glob(os.path.join(dataset_dir, "input/*.npy")))
         if "SA" in self.type:
             self.output_list = sorted(glob.glob(os.path.join(dataset_dir, "SA/*.npy")))
         else:
             self.output_list = sorted(glob.glob(os.path.join(dataset_dir, "output/*.npy")))
+            
+        X_train, X_test, y_train, y_test = train_test_split(self.input_list, self.output_list, test_size=0.2, random_state=42)
+        X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.125, random_state=42)
+        self.train_dataset = RFFullDataset3d(X_train, y_train, self.model.scaler)
+        self.valid_dataset = RFFullDataset3d(X_valid, y_valid, self.model.scaler)
+        self.test_dataset = RFFullDataset3d(X_test, y_test, self.model.scaler)
+        
         self.train_loss = []
         self.valid_loss = []
         return
     
     def train(self):
-        dataloader_train = DataLoader(self.traindataset, self.batch_size, 
+        dataloader_train = DataLoader(self.train_dataset, self.batch_size, 
                                 shuffle=True)
         
-        dataloader_valid = DataLoader(self.testdataset, self.batch_size, 
+        dataloader_valid = DataLoader(self.valid_dataset, self.batch_size, 
                                 shuffle=True)
         all_params = self.model.get_params()
         optimizer = torch.optim.Adam(all_params, lr=self.lr, weight_decay=self.l2_alpha)
@@ -88,12 +109,14 @@ class Trainer_prototype():
                 
                 clf_loss = criterion(pred, label)
                 valid_loss.append(clf_loss.item())
-                if i > 3000: break
             
             self.train_loss.append(np.mean(train_loss))
             self.valid_loss.append(np.mean(valid_loss))
-            print("Epoch: {}, train_loss: {}, valid_loss: {}".format(epoch, self.train_loss[-1], self.valid_loss[-1]))
-            if (epoch+1) % 5 == 0:
+            
+            if i % 10 == 0:
+                print("Epoch: {}, train_loss: {}, valid_loss: {}".format(epoch, self.train_loss[-1], self.valid_loss[-1]))
+                
+            if (epoch+1) % 2 == 0:
                 name = 'raw_data_epoch_'+str(epoch)+'.pth'
                 self.model.save_model(self.output_path, self.type, name)
                 print('Model saved')
@@ -101,7 +124,7 @@ class Trainer_prototype():
 
 
     def test(self, model_params=None):
-        dataloader = DataLoader(self.testdataset, self.batch_size, 
+        dataloader = DataLoader(self.test_dataset, self.batch_size, 
                                 shuffle=True)
         if model_params:
             self.model.reload_params(model_params)
@@ -148,107 +171,73 @@ class Trainer_3d_transfer(Trainer_prototype):
     
     def __init__(self, dataset_dir, scaler_path, model_output_path, lr=1e-3, epochs=100, batch_size=32, l2_alpha=1e-3, type="3d_transfer", random_state=42, params=None):
         
-        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, type, random_state)
-        
         if params:
             self.model = Conv_3d_transfer(scaler_path, params)
         else:
             self.model = Conv_3d_transfer(scaler_path)
-        
-        X_train, X_test, y_train, y_test = train_test_split(self.input_list, self.output_list, test_size=0.2, random_state=42)
-        self.traindataset = RFFullDataset3d(X_train, y_train, self.model.scaler)
-        self.testdataset = RFFullDataset3d(X_test, y_test, self.model.scaler)
-        
+            
+        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, type, random_state)
+            
         return
+    
     
 class Trainer_3d_VGG_transfer(Trainer_prototype):
     
     def __init__(self, dataset_dir, scaler_path, model_output_path, lr=1e-3, epochs=100, batch_size=8, l2_alpha=1e-3, type="3d_VGG_transfer", random_state=42, params=None):
-        
-        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, type, random_state)
         
         if params:
             self.model = Conv_3d_VGG_transfer(scaler_path, params)
         else:
             self.model = Conv_3d_VGG_transfer(scaler_path)
         
-        X_train, X_test, y_train, y_test = train_test_split(self.input_list, self.output_list, test_size=0.2, random_state=42)
-        self.traindataset = RFFullDataset3d(X_train, y_train, self.model.scaler)
-        self.testdataset = RFFullDataset3d(X_test, y_test, self.model.scaler)
+        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, type, random_state)
         
         return 
+    
     
 class Trainer_3d_SA(Trainer_prototype):
     
     def __init__(self, dataset_dir, scaler_path, model_output_path, lr=1e-3, epochs=100, batch_size=16, l2_alpha=1e-3, type="3d_SA", random_state=42):
         
-        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, type, random_state)
-        
         self.model = Conv_3d_SA(scaler_path)
         
-        X_train, X_test, y_train, y_test = train_test_split(self.input_list, self.output_list, test_size=0.2, random_state=42)
-        self.traindataset = RFFullDataset3d(X_train, y_train, self.model.scaler)
-        self.testdataset = RFFullDataset3d(X_test, y_test, self.model.scaler)
+        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, type, random_state)
         
         return     
 
+    
 class Trainer_3d_VGG_SA(Trainer_prototype):
     
     def __init__(self, dataset_dir, scaler_path, model_output_path, lr=1e-3, epochs=100, batch_size=8, l2_alpha=1e-3, type="3d_VGG_SA", random_state=42):
         
-        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, type, random_state)
-        
         self.model = Conv_3d_VGG_SA(scaler_path)
         
-        X_train, X_test, y_train, y_test = train_test_split(self.input_list, self.output_list, test_size=0.2, random_state=42)
-        self.traindataset = RFFullDataset3d(X_train, y_train, self.model.scaler)
-        self.testdataset = RFFullDataset3d(X_test, y_test, self.model.scaler)
+        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, type, random_state)
         
         return 
 
+    
 class Trainer_3d_VGG(Trainer_prototype):
     
-    def __init__(self, dataset_dir, scaler_path, model_output_path, lr=1e-3, epochs=100, batch_size=8, l2_alpha=1e-3, random_state=42):
-        
-        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, "3d_VGG", random_state)
+    def __init__(self, dataset_dir, scaler_path, model_output_path, lr=1e-3, epochs=100, batch_size=8, l2_alpha=1e-3, type="3d_VGG", random_state=42):
         
         self.model = Conv_3d_VGG(scaler_path)
         
-        X_train, X_test, y_train, y_test = train_test_split(self.input_list, self.output_list, test_size=0.2, random_state=42)
-        self.traindataset = RFFullDataset3d(X_train, y_train, self.model.scaler)
-        self.testdataset = RFFullDataset3d(X_test, y_test, self.model.scaler)
+        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, type, random_state)
         
         return 
     
     
 class Trainer_3d(Trainer_prototype):
     
-    def __init__(self, dataset_dir, scaler_path, model_output_path, lr=1e-3, epochs=100, batch_size=8, l2_alpha=1e-3, random_state=42):
-        
-        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, "3d", random_state)
+    def __init__(self, dataset_dir, scaler_path, model_output_path, lr=1e-3, epochs=100, batch_size=8, l2_alpha=1e-3, type="3d", random_state=42):
         
         self.model = Conv_3d(scaler_path)
         
-        X_train, X_test, y_train, y_test = train_test_split(self.input_list, self.output_list, test_size=0.2, random_state=42)
-        self.traindataset = RFFullDataset3d(X_train, y_train, self.model.scaler)
-        self.testdataset = RFFullDataset3d(X_test, y_test, self.model.scaler)
+        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, type, random_state)
         
         return 
-    
-    
-class Trainer_2d(Trainer_prototype):
-    
-    def __init__(self, dataset_dir, AE_weight_path, scaler_path, model_output_path, lr=1e-3, epochs=100, batch_size=32, l2_alpha=1e-3, random_state=42):
-        
-        Trainer_prototype.__init__(self, dataset_dir, model_output_path, lr, epochs, batch_size, l2_alpha, "2d", random_state)
-    
-        self.model = Conv_2d(AE_weight_path, scaler_path)
-    
-        X_train, X_test, y_train, y_test = train_test_split(self.input_list, self.output_list, test_size=0.2, random_state=random_state)
-        self.traindataset = RFFullDataset(X_train, y_train, self.model.scaler)
-        self.testdataset = RFFullDataset(X_test, y_test, self.model.scaler)
-        
-        return 
+  
     
 
     
